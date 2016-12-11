191422c4ccf1fef693582e7f18bc263e6f2e09d2
Richard Musiol <mail@richard-musiol.de>
2016-12-11 19:12:11 +0100

    merged code for value coercion and packing

--- internal/exec/packer.go
+++ internal/exec/packer.go
@@ -7,10 +7,11 @@ import (
 
 	"github.com/neelance/graphql-go/internal/common"
 	"github.com/neelance/graphql-go/internal/schema"
+	"github.com/pkg/errors"
 )
 
 type packer interface {
-	pack(value interface{}) reflect.Value
+	pack(r *request, value interface{}) (reflect.Value, error)
 }
 
 func makePacker(s *schema.Schema, schemaType common.Type, hasDefault bool, reflectType reflect.Type) (packer, error) {
@@ -28,9 +29,11 @@ func makePacker(s *schema.Schema, schemaType common.Type, hasDefault bool, refle
 			return nil, fmt.Errorf("wrong type, expected %s", want)
 		}
 		return &valuePacker{
-			valueType: reflectType,
+			scalar:    t,
 			nonNull:   nonNull,
+			valueType: reflectType,
 		}, nil
+
 	case *schema.Enum:
 		want := reflect.TypeOf("")
 		if !nonNull {
@@ -40,15 +43,18 @@ func makePacker(s *schema.Schema, schemaType common.Type, hasDefault bool, refle
 			return nil, fmt.Errorf("wrong type, expected %s", want)
 		}
 		return &valuePacker{
-			valueType: reflectType,
+			scalar:    stringScalar,
 			nonNull:   nonNull,
+			valueType: reflectType,
 		}, nil
+
 	case *schema.InputObject:
 		e, err := makeStructPacker(s, &t.InputMap, reflectType)
 		if err != nil {
 			return nil, err
 		}
 		return e, nil
+
 	case *common.List:
 		if reflectType.Kind() != reflect.Slice {
 			return nil, fmt.Errorf("expected slice, got %s", reflectType)
@@ -61,8 +67,10 @@ func makePacker(s *schema.Schema, schemaType common.Type, hasDefault bool, refle
 			sliceType: reflectType,
 			elem:      elem,
 		}, nil
+
 	case *schema.Object, *schema.Interface, *schema.Union:
 		return nil, fmt.Errorf("type of kind %s can not be used as input", t.Kind())
+
 	default:
 		panic("unreachable")
 	}
@@ -97,11 +105,11 @@ func makeStructPacker(s *schema.Schema, obj *common.InputMap, typ reflect.Type) 
 		fe.fieldPacker = p
 
 		if f.Default != nil {
-			defaultValue, err := coerceValue(nil, f.Type, f.Default)
+			v, err := fe.fieldPacker.pack(nil, f.Default)
 			if err != nil {
 				return nil, err
 			}
-			defaultStruct.FieldByIndex(fe.fieldIndex).Set(fe.fieldPacker.pack(defaultValue))
+			defaultStruct.FieldByIndex(fe.fieldIndex).Set(v)
 		}
 
 		fields = append(fields, fe)
@@ -126,17 +134,24 @@ type structPackerField struct {
 	fieldPacker packer
 }
 
-func (p *structPacker) pack(value interface{}) reflect.Value {
+func (p *structPacker) pack(r *request, value interface{}) (reflect.Value, error) {
+	if v, ok := value.(common.Variable); ok {
+		value = r.vars[string(v)]
+	}
+
 	values := value.(map[string]interface{})
 	v := reflect.New(p.structType)
 	v.Elem().Set(p.defaultStruct)
 	for _, f := range p.fields {
 		if value, ok := values[f.name]; ok {
-			fv := f.fieldPacker.pack(value)
-			v.Elem().FieldByIndex(f.fieldIndex).Set(fv)
+			packed, err := f.fieldPacker.pack(r, value)
+			if err != nil {
+				return reflect.Value{}, err
+			}
+			v.Elem().FieldByIndex(f.fieldIndex).Set(packed)
 		}
 	}
-	return v
+	return v, nil
 }
 
 type listPacker struct {
@@ -144,28 +159,49 @@ type listPacker struct {
 	elem      packer
 }
 
-func (e *listPacker) pack(value interface{}) reflect.Value {
+func (e *listPacker) pack(r *request, value interface{}) (reflect.Value, error) {
+	if v, ok := value.(common.Variable); ok {
+		value = r.vars[string(v)]
+	}
+
 	list := value.([]interface{})
 	v := reflect.MakeSlice(e.sliceType, len(list), len(list))
 	for i := range list {
-		v.Index(i).Set(e.elem.pack(list[i]))
+		packed, err := e.elem.pack(r, list[i])
+		if err != nil {
+			return reflect.Value{}, err
+		}
+		v.Index(i).Set(packed)
 	}
-	return v
+	return v, nil
 }
 
 type valuePacker struct {
-	valueType reflect.Type
+	scalar    *scalar
 	nonNull   bool
+	valueType reflect.Type
 }
 
-func (p *valuePacker) pack(value interface{}) reflect.Value {
-	if !p.nonNull {
-		if value == nil {
-			return reflect.Zero(p.valueType)
-		}
-		ptr := reflect.New(p.valueType.Elem())
-		ptr.Elem().Set(reflect.ValueOf(value))
-		return ptr
+func (p *valuePacker) pack(r *request, value interface{}) (reflect.Value, error) {
+	if v, ok := value.(common.Variable); ok {
+		value = r.vars[string(v)]
 	}
-	return reflect.ValueOf(value)
+
+	if value == nil {
+		if p.nonNull {
+			return reflect.Value{}, errors.Errorf("got null for non-null")
+		}
+		return reflect.Zero(p.valueType), nil
+	}
+
+	coerced, err := p.scalar.coerceInput(value)
+	if err != nil {
+		return reflect.Value{}, fmt.Errorf("could not convert %#v (%T) to %s: %s", value, value, p.scalar.name, err)
+	}
+	if !p.nonNull {
+		ptr := reflect.New(p.valueType.Elem())
+		ptr.Elem().Set(reflect.ValueOf(coerced))
+		return ptr, nil
+	}
+	return reflect.ValueOf(coerced), nil
 }

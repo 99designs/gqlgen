91bd7f887b6c9e45856c74db19e2081de48d1e60
Richard Musiol <mail@richard-musiol.de>
2017-03-10 15:58:25 +0100

    improved meta schema

--- internal/schema/meta.go
+++ internal/schema/meta.go
@@ -3,23 +3,29 @@ package schema
 var Meta *Schema
 
 func init() {
-	Meta = &Schema{
-		entryPointNames: make(map[string]string),
-		Types: map[string]NamedType{
-			"Int":     &Scalar{"Int", "The `Int` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1."},
-			"Float":   &Scalar{"Float", "The `Float` scalar type represents signed double-precision fractional values as specified by [IEEE 754](http://en.wikipedia.org/wiki/IEEE_floating_point)."},
-			"String":  &Scalar{"String", "The `String` scalar type represents textual data, represented as UTF-8 character sequences. The String type is most often used by GraphQL to represent free-form human-readable text."},
-			"Boolean": &Scalar{"Boolean", "The `Boolean` scalar type represents `true` or `false`."},
-			"ID":      &Scalar{"ID", "The `ID` scalar type represents a unique identifier, often used to refetch an object or as key for a cache. The ID type appears in a JSON response as a String; however, it is not intended to be human-readable. When expected as an input type, any string (such as `\"4\"`) or integer (such as `4`) input value will be accepted as an ID."},
-		},
-		Directives: make(map[string]*Directive),
-	}
+	Meta = &Schema{} // bootstrap
+	Meta = New()
 	if err := Meta.Parse(metaSrc); err != nil {
 		panic(err)
 	}
 }
 
 var metaSrc = `
+	# The ` + "`" + `Int` + "`" + ` scalar type represents non-fractional signed whole numeric values. Int can represent values between -(2^31) and 2^31 - 1.
+	scalar Int
+
+	# The ` + "`" + `Float` + "`" + ` scalar type represents signed double-precision fractional values as specified by [IEEE 754](http://en.wikipedia.org/wiki/IEEE_floating_point).
+	scalar Float
+
+	# The ` + "`" + `String` + "`" + ` scalar type represents textual data, represented as UTF-8 character sequences. The String type is most often used by GraphQL to represent free-form human-readable text.
+	scalar String
+
+	# The ` + "`" + `Boolean` + "`" + ` scalar type represents ` + "`" + `true` + "`" + ` or ` + "`" + `false` + "`" + `.
+	scalar Boolean
+
+	# The ` + "`" + `ID` + "`" + ` scalar type represents a unique identifier, often used to refetch an object or as key for a cache. The ID type appears in a JSON response as a String; however, it is not intended to be human-readable. When expected as an input type, any string (such as ` + "`" + `"4"` + "`" + `) or integer (such as ` + "`" + `4` + "`" + `) input value will be accepted as an ID.
+	scalar ID
+
 	# Directs the executor to include this field or fragment only when the ` + "`" + `if` + "`" + ` argument is true.
 	directive @include(
 		# Included when true.

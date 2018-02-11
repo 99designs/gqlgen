fb2d5817ebb2c447880b5b3525b56954ca9c742a
Adam Scarr <adam@vektah.net>
2018-02-11 22:08:51 +1100

    use gorunpkg to vendor go generate binaries

--- example/dataloader/dataloaders.go
+++ example/dataloader/dataloaders.go
@@ -1,6 +1,6 @@
-//go:generate dataloaden -keys int github.com/vektah/gqlgen/example/dataloader.Address
-//go:generate dataloaden -keys int -slice github.com/vektah/gqlgen/example/dataloader.Order
-//go:generate dataloaden -keys int -slice github.com/vektah/gqlgen/example/dataloader.Item
+//go:generate gorunpkg github.com/vektah/dataloaden -keys int github.com/vektah/gqlgen/example/dataloader.Address
+//go:generate gorunpkg github.com/vektah/dataloaden -keys int -slice github.com/vektah/gqlgen/example/dataloader.Order
+//go:generate gorunpkg github.com/vektah/dataloaden -keys int -slice github.com/vektah/gqlgen/example/dataloader.Item
 
 package dataloader
 
--- example/starwars/starwars.go
+++ example/starwars/starwars.go
@@ -1,4 +1,4 @@
-//go:generate gqlgen -out generated.go
+//go:generate gorunpkg github.com/vektah/gqlgen -out generated.go
 
 package starwars
 

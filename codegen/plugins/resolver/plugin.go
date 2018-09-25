fab2abbf1492de8961c79f75338d799b875ee6b1
Mathew Byrne <mathew.byrne@99designs.com>
2018-09-25 13:19:13 +1000

    Refactor directive fetch

--- codegen/plugins/resolver/plugin.go
+++ codegen/plugins/resolver/plugin.go
@@ -13,8 +13,8 @@ type Plugin struct{}
 
 var plugin = &Plugin{}
 
-func (p *Plugin) Schema(cfg *codegen.Config) (ast.Source, error) {
-	return ast.Source{
+func (p *Plugin) Schema(cfg *codegen.Config) (*ast.Source, error) {
+	return &ast.Source{
 		Name:  "resolver plugin",
 		Input: "directive @resolver on FIELD_DEFINITION",
 	}, nil
@@ -27,20 +27,18 @@ func (p *Plugin) Execute(cfg *codegen.Config, schema *ast.Schema) error {
 	cfg.Directives["resolver"] = codegen.DirectiveMapEntry{Implementation: "github.com/99designs/gqlgen/codegen/plugins/resolver.DirectiveNoop"}
 	for _, typ := range schema.Types {
 		for _, f := range typ.Fields {
-			for _, d := range f.Directives {
-				if d.Name == "resolver" {
-					if _, ok := cfg.Models[typ.Name]; !ok {
-						cfg.Models[typ.Name] = codegen.TypeMapEntry{}
+			if d := f.Directives.ForName("resolver"); d != nil {
+				if _, ok := cfg.Models[typ.Name]; !ok {
+					cfg.Models[typ.Name] = codegen.TypeMapEntry{}
+				}
+				if tmf, ok := cfg.Models[typ.Name].Fields[f.Name]; ok {
+					cfg.Models[typ.Name].Fields[f.Name] = codegen.TypeMapField{
+						ForceResolver: true,
+						FieldName:     tmf.FieldName,
 					}
-					if tmf, ok := cfg.Models[typ.Name].Fields[f.Name]; ok {
-						cfg.Models[typ.Name].Fields[f.Name] = codegen.TypeMapField{
-							ForceResolver: true,
-							FieldName:     tmf.FieldName,
-						}
-					} else {
-						cfg.Models[typ.Name].Fields[f.Name] = codegen.TypeMapField{
-							ForceResolver: true,
-						}
+				} else {
+					cfg.Models[typ.Name].Fields[f.Name] = codegen.TypeMapField{
+						ForceResolver: true,
 					}
 				}
 			}

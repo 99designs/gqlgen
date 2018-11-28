d22f03c62d582f36872c85507b75d095c0ec4fc9
Mathew Byrne <mathew.byrne@99designs.com>
2018-11-28 13:28:42 +1100

    Add deprecation warning

--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "github.com/99designs/gqlgen/cmd"
+import (
+	"fmt"
+	"os"
+
+	"github.com/99designs/gqlgen/cmd"
+)
 
 func main() {
+	fmt.Fprintf(os.Stderr, "warning: running gqlgen from this binary is deprecated and may be removed in a future release. See https://github.com/99designs/gqlgen/issues/415\n")
 	cmd.Execute()
 }

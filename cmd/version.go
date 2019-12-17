3bf6bf663595e97dc044666f633ffd8d736554c1
Richard Lindhout <richardlindhout96@gmail.com>
2019-12-17 15:19:37 +0100

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -7,10 +7,11 @@ import (
 	"github.com/urfave/cli"
 )
 
-var versionCmd = cli.Command{
+var versionCmd = &cli.Command{
 	Name:  "version",
 	Usage: "print the version string",
-	Action: func(ctx *cli.Context) {
+	Action: func(ctx *cli.Context) error {
 		fmt.Println(graphql.Version)
+		return nil
 	},
 }

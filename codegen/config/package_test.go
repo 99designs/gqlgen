c233876e67aea3946d73890aeb9b96cfc4106edd
Adam <adam@vektah.net>
2020-02-03 11:52:37 +1100

    fix windows test paths

--- codegen/config/package_test.go
+++ codegen/config/package_test.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -19,7 +20,7 @@ func TestPackageConfig(t *testing.T) {
 		require.Equal(t, "config_test_data", p.Pkg().Name())
 		require.Equal(t, "github.com/99designs/gqlgen/codegen/config/testdata", p.Pkg().Path())
 
-		require.Contains(t, p.Filename, "codegen/config/testdata/example.go")
+		require.Contains(t, filepath.ToSlash(p.Filename), "codegen/config/testdata/example.go")
 		require.Contains(t, p.Dir(), "codegen/config/testdata")
 	})
 
@@ -35,7 +36,7 @@ func TestPackageConfig(t *testing.T) {
 		require.Equal(t, "wololo", p.Pkg().Name())
 		require.Equal(t, "github.com/99designs/gqlgen/codegen/config/testdata", p.Pkg().Path())
 
-		require.Contains(t, p.Filename, "codegen/config/testdata/example.go")
+		require.Contains(t, filepath.ToSlash(p.Filename), "codegen/config/testdata/example.go")
 		require.Contains(t, p.Dir(), "codegen/config/testdata")
 	})
 

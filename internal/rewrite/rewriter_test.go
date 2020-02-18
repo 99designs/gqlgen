3abe5b32e965a6a05164a66110ac7cafda2e89d3
vvakame <vvakame+dev@gmail.com>
2020-02-18 14:16:24 +0900

    add test

--- internal/rewrite/rewriter_test.go
+++ internal/rewrite/rewriter_test.go
@@ -8,29 +8,37 @@ import (
 )
 
 func TestRewriter(t *testing.T) {
-	r, err := New("github.com/99designs/gqlgen/internal/rewrite/testdata")
-	require.NoError(t, err)
+	t.Run("default", func(t *testing.T) {
+		r, err := New("github.com/99designs/gqlgen/internal/rewrite/testdata")
+		require.NoError(t, err)
 
-	body := r.GetMethodBody("Foo", "Method")
-	require.Equal(t, `
-	// leading comment
+		body := r.GetMethodBody("Foo", "Method")
+		require.Equal(t, `
+			// leading comment
 
-	// field comment
-	m.Field++
+			// field comment
+			m.Field++
 
-	// trailing comment
-`, body)
+			// trailing comment
+		`, body)
 
-	imps := r.ExistingImports("testdata/example.go")
-	require.Len(t, imps, 2)
-	assert.Equal(t, []Import{
-		{
-			Alias:      "",
-			ImportPath: "fmt",
-		},
-		{
-			Alias:      "lol",
-			ImportPath: "bytes",
-		},
-	}, imps)
+		imps := r.ExistingImports("testdata/example.go")
+		require.Len(t, imps, 2)
+		assert.Equal(t, []Import{
+			{
+				Alias:      "",
+				ImportPath: "fmt",
+			},
+			{
+				Alias:      "lol",
+				ImportPath: "bytes",
+			},
+		}, imps)
+
+	})
+
+	t.Run("out of scope dir", func(t *testing.T) {
+		_, err := New("../../../out-of-gomod/package")
+		require.Error(t, err)
+	})
 }

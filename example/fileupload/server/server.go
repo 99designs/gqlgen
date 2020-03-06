697aa0b1c0d93ba14e5f1f8f21dab7b03f02a497
kskumgk63 <keisuke.umegaki.630@gmail.com>
2020-03-06 13:51:17 +0900

    fix: error when starting example app

--- example/fileupload/server/server.go
+++ example/fileupload/server/server.go
@@ -7,28 +7,18 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/99designs/gqlgen/graphql/handler/transport"
-
-	"github.com/99designs/gqlgen/graphql/playground"
-
 	"github.com/99designs/gqlgen/example/fileupload"
 	"github.com/99designs/gqlgen/example/fileupload/model"
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 func main() {
 	http.Handle("/", playground.Handler("File Upload Demo", "/query"))
 	resolver := getResolver()
 
-	var mb int64 = 1 << 20
-
-	srv := handler.New(fileupload.NewExecutableSchema(fileupload.Config{Resolvers: resolver}))
-
-	srv.AddTransport(transport.MultipartForm{
-		MaxMemory:     32 * mb,
-		MaxUploadSize: 50 * mb,
-	})
+	srv := handler.NewDefaultServer(fileupload.NewExecutableSchema(fileupload.Config{Resolvers: resolver}))
 
 	http.Handle("/query", srv)
 	log.Print("connect to http://localhost:8087/ for GraphQL playground")
@@ -44,9 +34,10 @@ func getResolver() *fileupload.Stub {
 			return nil, err
 		}
 		return &model.File{
-			ID:      1,
-			Name:    file.Filename,
-			Content: string(content),
+			ID:          1,
+			Name:        file.Filename,
+			Content:     string(content),
+			ContentType: file.ContentType,
 		}, nil
 	}
 	resolver.MutationResolver.SingleUploadWithPayload = func(ctx context.Context, req model.UploadFile) (*model.File, error) {
@@ -55,9 +46,10 @@ func getResolver() *fileupload.Stub {
 			return nil, err
 		}
 		return &model.File{
-			ID:      1,
-			Name:    req.File.Filename,
-			Content: string(content),
+			ID:          1,
+			Name:        req.File.Filename,
+			Content:     string(content),
+			ContentType: req.File.ContentType,
 		}, nil
 	}
 	resolver.MutationResolver.MultipleUpload = func(ctx context.Context, files []*graphql.Upload) ([]*model.File, error) {
@@ -65,36 +57,41 @@ func getResolver() *fileupload.Stub {
 			return nil, errors.New("empty list")
 		}
 		var resp []*model.File
-		for i := range files {
-			content, err := ioutil.ReadAll(files[i].File)
+		for i, file := range files {
+			content, err := ioutil.ReadAll(file.File)
 			if err != nil {
 				return []*model.File{}, err
 			}
 			resp = append(resp, &model.File{
-				ID:      i + 1,
-				Name:    files[i].Filename,
-				Content: string(content),
+				ID:          i + 1,
+				Name:        file.Filename,
+				Content:     string(content),
+				ContentType: file.ContentType,
 			})
 		}
 		return resp, nil
 	}
-	resolver.MutationResolver.MultipleUploadWithPayload = func(ctx context.Context, req []*model.UploadFile) ([]*model.File, error) {
-		if len(req) == 0 {
+	resolver.MutationResolver.MultipleUploadWithPayload = func(ctx context.Context, reqs []*model.UploadFile) ([]*model.File, error) {
+		if len(reqs) == 0 {
 			return nil, errors.New("empty list")
 		}
 		var resp []*model.File
-		for i := range req {
-			content, err := ioutil.ReadAll(req[i].File.File)
+		for i, req := range reqs {
+			content, err := ioutil.ReadAll(req.File.File)
 			if err != nil {
 				return []*model.File{}, err
 			}
 			resp = append(resp, &model.File{
-				ID:      i + 1,
-				Name:    req[i].File.Filename,
-				Content: string(content),
+				ID:          i + 1,
+				Name:        req.File.Filename,
+				Content:     string(content),
+				ContentType: req.File.ContentType,
 			})
 		}
 		return resp, nil
+	}
+	resolver.QueryResolver.Empty = func(ctx context.Context) (s string, err error) {
+		return "empty", nil
 	}
 	return resolver
 }

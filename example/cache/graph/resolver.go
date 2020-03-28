b852466e91d49a5ce15227b295fe964bd17fe138
Leonardo Andrade <leonardo.andrade@corp.globo.com>
2020-03-28 00:18:05 -0300

    delete example/cache

--- example/cache/graph/resolver.go
+++
@@ -1,6 +0,0 @@
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
-package graph
-
-type Resolver struct{}

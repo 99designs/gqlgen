e7798ff26198eceb79f10d5743558aba02629ec8
Gavi Galloway <gsgalloway@users.noreply.github.com>
2020-04-13 12:18:38 -0700

    insert operation context

--- graphql/handler/transport/http_post.go
+++ graphql/handler/transport/http_post.go
@@ -48,6 +48,7 @@ func (h POST) Do(w http.ResponseWriter, r *http.Request, exec graphql.GraphExecu
 		writeJson(w, resp)
 		return
 	}
+	ctx = graphql.WithOperationContext(r.Context(), rc)
 	responses, ctx := exec.DispatchOperation(r.Context(), rc)
 	writeJson(w, responses(ctx))
 }

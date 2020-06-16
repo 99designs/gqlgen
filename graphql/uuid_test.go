81f880df9d8d0a13701aa9ecbc087b3952edeb1f
Nur Kutlugallyamov <nurkng@yandex.ru>
2020-06-16 09:37:44 +0300

    add test for unmarshal uuid

--- graphql/uuid_test.go
+++ graphql/uuid_test.go
@@ -1,6 +1,7 @@
 package graphql
 
 import (
+	"fmt"
 	"strconv"
 	"testing"
 
@@ -27,5 +28,31 @@ func TestUUID(t *testing.T) {
 
 	for _, v := range uuidTD {
 		assert.Equal(t, strconv.Quote(v.uuid.String()), m2s(MarshalUUID(v.uuid)))
+	}
+}
+
+func TestUnmarshalUUID(t *testing.T) {
+	uuidTD := []struct {
+		isGoodCase bool
+		uuid       interface{}
+	}{
+		{isGoodCase: true, uuid: "12345678-1234-1234-1234-123456789012"},
+		{isGoodCase: true, uuid: "{12345678-1234-1234-1234-123456789012}"},
+		{isGoodCase: true, uuid: "urn:uuid:12345678-1234-1234-1234-123456789012"},
+		{isGoodCase: true, uuid: "12345678901234567890123456789012"},
+		{isGoodCase: false, uuid: ""},
+		{isGoodCase: false, uuid: "1234567890123456789012"},
+		{isGoodCase: false, uuid: "z2345678901234567890123456789012"},
+		{isGoodCase: false, uuid: 42},
+	}
+
+	for i, v := range uuidTD {
+		u, err := UnmarshalUUID(v.uuid)
+		assert.NotNil(t, u)
+		if v.isGoodCase {
+			assert.NoError(t, err, fmt.Sprintf("case #%d", i))
+		} else {
+			assert.Error(t, err, fmt.Sprintf("case #%d", i))
+		}
 	}
 }

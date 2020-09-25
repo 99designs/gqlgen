50a80ab3e9dcea84bb520f4869d57aa7f4625979
Luke Cawood <luke.cawood@99designs.com>
2020-09-25 10:58:26 +1000

    Generate an example test during init

--- plugin/servergen/server.go
+++ plugin/servergen/server.go
@@ -3,12 +3,32 @@ package servergen
 import (
 	"log"
 	"os"
+	"strings"
 
 	"github.com/99designs/gqlgen/codegen"
 	"github.com/99designs/gqlgen/codegen/templates"
 	"github.com/99designs/gqlgen/plugin"
 	"github.com/pkg/errors"
 )
+
+const testTemplate = `
+{{ reserveImport "testing" }}
+{{ reserveImport "github.com/99designs/gqlgen/client" }}
+
+func TestIntrospectTodo(t *testing.T) {
+	var resp struct {
+		Type struct {
+			Kind string
+		}
+	}
+	c := client.New(createHandler(&{{ lookupImport .ResolverPackageName}}.Resolver{}))
+	c.MustPost("query ($type: String!){ type:__type(name: $type) { kind } }", &resp, client.Var("type", "Todo"))
+
+	if resp.Type.Kind != "OBJECT" {
+		t.Error("Unexpected kind for TODO. Expected OBJECT, found " + resp.Type.Kind)
+	}
+}
+`
 
 func New(filename string) plugin.Plugin {
 	return &Plugin{filename}
@@ -30,6 +50,16 @@ func (m *Plugin) GenerateCode(data *codegen.Data) error {
 	}
 
 	if _, err := os.Stat(m.filename); os.IsNotExist(errors.Cause(err)) {
+		err := templates.Render(templates.Options{
+			PackageName: "main",
+			Filename:    strings.Replace(m.filename, ".go", "_test.go", 1),
+			Data:        serverBuild,
+			Packages:    data.Config.Packages,
+			Template:    testTemplate,
+		})
+		if err != nil {
+			return err
+		}
 		return templates.Render(templates.Options{
 			PackageName: "main",
 			Filename:    m.filename,

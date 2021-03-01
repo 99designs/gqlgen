d429aa869aa480a5f7097b289f833431606288d2
Abel Tay <abel.tay@gmail.com>
2021-03-01 14:23:45 +0800

    Upgrade go-chi to v5

--- example/dataloader/server/server.go
+++ example/dataloader/server/server.go
@@ -7,7 +7,7 @@ import (
 	"github.com/99designs/gqlgen/example/dataloader"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5"
 )
 
 func main() {

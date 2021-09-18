968e0f62c83a416d827b08440d7d8800abcff9ed
wilhelm <helm@hey.com>
2021-09-18 20:01:56 +1000

    Restore Accidentally Cut Test

--- codegen/testserver/input_test.go
+++ codegen/testserver/input_test.go
@@ -14,6 +14,21 @@ func TestInput(t *testing.T) {
 	srv := handler.NewDefaultServer(NewExecutableSchema(Config{Resolvers: resolvers}))
 	c := client.New(srv)
 
+	t.Run("when function errors on directives", func(t *testing.T) {
+		resolvers.QueryResolver.InputSlice = func(ctx context.Context, arg []string) (b bool, e error) {
+			return true, nil
+		}
+
+		var resp struct {
+			DirectiveArg *string
+		}
+
+		err := c.Post(`query { inputSlice(arg: ["ok", 1, 2, "ok"]) }`, &resp)
+
+		require.EqualError(t, err, `http 422: {"errors":[{"message":"Expected type String!, found 1.","locations":[{"line":1,"column":32}],"extensions":{"code":"GRAPHQL_VALIDATION_FAILED"}},{"message":"Expected type String!, found 2.","locations":[{"line":1,"column":35}],"extensions":{"code":"GRAPHQL_VALIDATION_FAILED"}}],"data":null}`)
+		require.Nil(t, resp.DirectiveArg)
+	})
+
 	t.Run("when input slice nullable", func(t *testing.T) {
 		resolvers.QueryResolver.InputNullableSlice = func(ctx context.Context, arg []string) (b bool, e error) {
 			return arg == nil, nil

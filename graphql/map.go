33605d317231dfde9c77f440dbea2c039181edf2
Bai Chan Kheo <baichan.kheo@99designs.com>
2021-10-04 15:11:27 +1100

    Test unmarshal

--- graphql/map.go
+++ graphql/map.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"strconv"
 )
 
 func MarshalMap(val map[string]interface{}) Marshaler {
@@ -16,9 +17,28 @@ func MarshalMap(val map[string]interface{}) Marshaler {
 }
 
 func UnmarshalMap(v interface{}) (map[string]interface{}, error) {
+	unmarshalNumber(v)
+
 	if m, ok := v.(map[string]interface{}); ok {
 		return m, nil
 	}
 
 	return nil, fmt.Errorf("%T is not a map", v)
+}
+
+func unmarshalNumber(v interface{}) {
+	if n, ok := v.(json.Number); ok {
+		v, _ = strconv.Atoi(string(n))
+	}
+
+	switch v := v.(type) {
+	case []interface{}:
+		for _, v := range v {
+			unmarshalNumber(v)
+		}
+	case map[string]interface{}:
+		for _, v := range v {
+			unmarshalNumber(v)
+		}
+	}
 }

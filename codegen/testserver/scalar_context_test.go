9dc0eacf1f23439ae7d5fa336335b831f30ca484
Jonathan Duck <duckbrain30@gmail.com>
2021-10-09 10:36:38 -0700

    Rename context test

--- codegen/testserver/scalar_context_test.go
+++ codegen/testserver/scalar_context_test.go
@@ -28,3 +28,21 @@ func TestFloatInfAndNaN(t *testing.T) {
 	})
 
 }
+func TestContextPassedToMarshal(t *testing.T) {
+	resolvers := &Stub{}
+
+	c := client.New(handler.NewDefaultServer(NewExecutableSchema(Config{Resolvers: resolvers})))
+
+	resolvers.QueryResolver.Rectangle = func(ctx context.Context) (*Rectangle, error) {
+		return &Rectangle{
+			Length: math.Inf(-1),
+			Width:  math.NaN(),
+		}, nil
+	}
+
+	t.Run("errors on marshaller with context", func(t *testing.T) {
+		err := c.Post(`query { rectangle { length width } }`, nil)
+		require.Error(t, err)
+	})
+
+}

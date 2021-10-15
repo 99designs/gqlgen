129783590c9cb8c709a2b0e9cbb7b69f022ee78a
Alex Sonneveld <Alex.sonneveld@99designs.com>
2021-10-15 21:40:03 +1100

    Update GQLgen test client to work with multipart form data (take 2) (#1661)
    * Update GQLgen test client to work with multipart form data
    
    Update the GQLgen to support multipart form data, like those present
    within the fileupload examples.
    
    - Add missing space between "unsupported encoding " and failing
      content-type header error
    
    (cherry picked from commit 101842f73fb79b10c1299bb40506080e08543ec6)
    
    * Add WithFiles client option for fileupload GQLgen client tests
    
    Add a `WithFiles` GQLgen client option to support the fileupload input
    within tests, using the core Golang `os` package and File type, which
    converts `os.File`s to their appropriate multipart form data within a
    request.
    
    - If there are no files this should just simply convert a
      `application/json` Content-Type to supported `multipart/form-data`
    
    (cherry picked from commit 08ef942416c98a2cadf61223308a3ff3c879d1c9)
    
    * Update fileupload test to use GQLgen test client
    
    Update the fileupload test to use the GQLgen test client and `WithFiles`
    option to remove the need for `createUploadRequest` helper with raw http
    posts
    
    - Fix setting the Content Type by using the appropriate `http` package
      function to dectect it
    
      + https://godoc.org/net/http#DetectContentType
    
    (cherry picked from commit 5e573d51440eba9d457adb4186772577b28ef085)
    
    * Update WithFiles option test with multipart Reader
    
    (cherry picked from commit 6dfa3cbe0647138e80a59a0c1d55dd9c900f96f2)
    
    * Update file upload tests `WithFiles` option
    
    Update the file upload tests to use the GQL test client and its
    `WithFiles` option to remove the need for a custom raw HTTP post request
    builder `createUploadRequest`.
    
    - Also update `WithFiles` option to group & map identical files; e.g.
    
      ```
        { "0": ["variables.req.0.file", "variables.req.1.file"] }
      ```
    
    (cherry picked from commit 486d9f1b2b200701f9ce6b386736a633547c1441)
    
    * Make sure `WithFiles` does not add duplicates to multipart form data
    
    (cherry picked from commit 0c2364d8495553051d97ab805618b006fcd9eddb)
    
    * Fix use of byte vs string in `WithFiles` tests
    
    (cherry picked from commit ba10b5b1c52a74e63e825ee57c235254e8821e0d)
    
    * Fix strict withFiles option test for race conditions
    
    Fix a problem with how strict the test's expected response was for tests
    with files in their request, since it always expected a strict order of
    files input that is somewhat random or dependent on what OS it is
    running the test on and/or race condition

---
+++ client/withfilesoption_test.go
@@ -0,0 +1,237 @@
+package client_test
+
+import (
+	"io"
+	"io/ioutil"
+	"mime"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"regexp"
+	"strings"
+	"testing"
+
+	"github.com/99designs/gqlgen/client"
+	"github.com/stretchr/testify/require"
+)
+
+func TestWithFiles(t *testing.T) {
+	tempFile1, _ := ioutil.TempFile(os.TempDir(), "tempFile1")
+	tempFile2, _ := ioutil.TempFile(os.TempDir(), "tempFile2")
+	tempFile3, _ := ioutil.TempFile(os.TempDir(), "tempFile3")
+	defer os.Remove(tempFile1.Name())
+	defer os.Remove(tempFile2.Name())
+	defer os.Remove(tempFile3.Name())
+	tempFile1.WriteString(`The quick brown fox jumps over the lazy dog`)
+	tempFile2.WriteString(`hello world`)
+	tempFile3.WriteString(`La-Li-Lu-Le-Lo`)
+
+	t.Run("with one file", func(t *testing.T) {
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+			require.NoError(t, err)
+			require.True(t, strings.HasPrefix(mediaType, "multipart/"))
+
+			mr := multipart.NewReader(r.Body, params["boundary"])
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				}
+				require.NoError(t, err)
+
+				slurp, err := ioutil.ReadAll(p)
+				require.NoError(t, err)
+
+				contentDisposition := p.Header.Get("Content-Disposition")
+
+				if contentDisposition == `form-data; name="operations"` {
+					require.EqualValues(t, `{"query":"{ id }","variables":{"file":{}}}`, slurp)
+				}
+				if contentDisposition == `form-data; name="map"` {
+					require.EqualValues(t, `{"0":["variables.file"]}`, slurp)
+				}
+				if regexp.MustCompile(`form-data; name="0"; filename=.*`).MatchString(contentDisposition) {
+					require.Equal(t, `text/plain; charset=utf-8`, p.Header.Get("Content-Type"))
+					require.EqualValues(t, `The quick brown fox jumps over the lazy dog`, slurp)
+				}
+			}
+			w.Write([]byte(`{}`))
+		})
+
+		c := client.New(h)
+
+		var resp struct{}
+		c.MustPost("{ id }", &resp,
+			client.Var("file", tempFile1),
+			client.WithFiles(),
+		)
+	})
+
+	t.Run("with multiple files", func(t *testing.T) {
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+			require.NoError(t, err)
+			require.True(t, strings.HasPrefix(mediaType, "multipart/"))
+
+			mr := multipart.NewReader(r.Body, params["boundary"])
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				}
+				require.NoError(t, err)
+
+				slurp, err := ioutil.ReadAll(p)
+				require.NoError(t, err)
+
+				contentDisposition := p.Header.Get("Content-Disposition")
+
+				if contentDisposition == `form-data; name="operations"` {
+					require.EqualValues(t, `{"query":"{ id }","variables":{"input":{"files":[{},{}]}}}`, slurp)
+				}
+				if contentDisposition == `form-data; name="map"` {
+					// returns `{"0":["variables.input.files.0"],"1":["variables.input.files.1"]}`
+					// but the order of file inputs is unpredictable between different OS systems
+					require.Contains(t, string(slurp), `{"0":`)
+					require.Contains(t, string(slurp), `["variables.input.files.0"]`)
+					require.Contains(t, string(slurp), `,"1":`)
+					require.Contains(t, string(slurp), `["variables.input.files.1"]`)
+					require.Contains(t, string(slurp), `}`)
+				}
+				if regexp.MustCompile(`form-data; name="[0,1]"; filename=.*`).MatchString(contentDisposition) {
+					require.Equal(t, `text/plain; charset=utf-8`, p.Header.Get("Content-Type"))
+					require.Contains(t, []string{
+						`The quick brown fox jumps over the lazy dog`,
+						`hello world`,
+					}, string(slurp))
+				}
+			}
+			w.Write([]byte(`{}`))
+		})
+
+		c := client.New(h)
+
+		var resp struct{}
+		c.MustPost("{ id }", &resp,
+			client.Var("input", map[string]interface{}{
+				"files": []*os.File{tempFile1, tempFile2},
+			}),
+			client.WithFiles(),
+		)
+	})
+
+	t.Run("with multiple files across multiple variables", func(t *testing.T) {
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+			require.NoError(t, err)
+			require.True(t, strings.HasPrefix(mediaType, "multipart/"))
+
+			mr := multipart.NewReader(r.Body, params["boundary"])
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				}
+				require.NoError(t, err)
+
+				slurp, err := ioutil.ReadAll(p)
+				require.NoError(t, err)
+
+				contentDisposition := p.Header.Get("Content-Disposition")
+
+				if contentDisposition == `form-data; name="operations"` {
+					require.EqualValues(t, `{"query":"{ id }","variables":{"req":{"files":[{},{}],"foo":{"bar":{}}}}}`, slurp)
+				}
+				if contentDisposition == `form-data; name="map"` {
+					// returns `{"0":["variables.req.files.0"],"1":["variables.req.files.1"],"2":["variables.req.foo.bar"]}`
+					// but the order of file inputs is unpredictable between different OS systems
+					require.Contains(t, string(slurp), `{"0":`)
+					require.Contains(t, string(slurp), `["variables.req.files.0"]`)
+					require.Contains(t, string(slurp), `,"1":`)
+					require.Contains(t, string(slurp), `["variables.req.files.1"]`)
+					require.Contains(t, string(slurp), `,"2":`)
+					require.Contains(t, string(slurp), `["variables.req.foo.bar"]`)
+					require.Contains(t, string(slurp), `}`)
+				}
+				if regexp.MustCompile(`form-data; name="[0,1,2]"; filename=.*`).MatchString(contentDisposition) {
+					require.Equal(t, `text/plain; charset=utf-8`, p.Header.Get("Content-Type"))
+					require.Contains(t, []string{
+						`The quick brown fox jumps over the lazy dog`,
+						`La-Li-Lu-Le-Lo`,
+						`hello world`,
+					}, string(slurp))
+				}
+			}
+			w.Write([]byte(`{}`))
+		})
+
+		c := client.New(h)
+
+		var resp struct{}
+		c.MustPost("{ id }", &resp,
+			client.Var("req", map[string]interface{}{
+				"files": []*os.File{tempFile1, tempFile2},
+				"foo": map[string]interface{}{
+					"bar": tempFile3,
+				},
+			}),
+			client.WithFiles(),
+		)
+	})
+
+	t.Run("with multiple files and file reuse", func(t *testing.T) {
+		h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			mediaType, params, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+			require.NoError(t, err)
+			require.True(t, strings.HasPrefix(mediaType, "multipart/"))
+
+			mr := multipart.NewReader(r.Body, params["boundary"])
+			for {
+				p, err := mr.NextPart()
+				if err == io.EOF {
+					break
+				}
+				require.NoError(t, err)
+
+				slurp, err := ioutil.ReadAll(p)
+				require.NoError(t, err)
+
+				contentDisposition := p.Header.Get("Content-Disposition")
+
+				if contentDisposition == `form-data; name="operations"` {
+					require.EqualValues(t, `{"query":"{ id }","variables":{"files":[{},{},{}]}}`, slurp)
+				}
+				if contentDisposition == `form-data; name="map"` {
+					require.EqualValues(t, `{"0":["variables.files.0","variables.files.2"],"1":["variables.files.1"]}`, slurp)
+					// returns `{"0":["variables.files.0","variables.files.2"],"1":["variables.files.1"]}`
+					// but the order of file inputs is unpredictable between different OS systems
+					require.Contains(t, string(slurp), `{"0":`)
+					require.Contains(t, string(slurp), `["variables.files.0"`)
+					require.Contains(t, string(slurp), `,"1":`)
+					require.Contains(t, string(slurp), `"variables.files.1"]`)
+					require.Contains(t, string(slurp), `"variables.files.2"]`)
+					require.NotContains(t, string(slurp), `,"2":`)
+					require.Contains(t, string(slurp), `}`)
+				}
+				if regexp.MustCompile(`form-data; name="[0,1]"; filename=.*`).MatchString(contentDisposition) {
+					require.Equal(t, `text/plain; charset=utf-8`, p.Header.Get("Content-Type"))
+					require.Contains(t, []string{
+						`The quick brown fox jumps over the lazy dog`,
+						`hello world`,
+					}, string(slurp))
+				}
+				require.False(t, regexp.MustCompile(`form-data; name="2"; filename=.*`).MatchString(contentDisposition))
+			}
+			w.Write([]byte(`{}`))
+		})
+
+		c := client.New(h)
+
+		var resp struct{}
+		c.MustPost("{ id }", &resp,
+			client.Var("files", []*os.File{tempFile1, tempFile2, tempFile1}),
+			client.WithFiles(),
+		)
+	})
+}

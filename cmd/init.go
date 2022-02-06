c75ee4928add2bb3d1c1707556256019288f5fb4
Zach <zach@Zachs-MacBook-Pro.local>
2022-02-05 18:57:02 -0800

    remove autobind example

--- cmd/init.go
+++ cmd/init.go
@@ -55,7 +55,7 @@ resolver:
 # gqlgen will search for any type names in the schema in these go packages
 # if they match it will use them, otherwise it will generate them.
 autobind:
-  - "{{.}}/graph/model"
+#  - "{{.}}/graph/model"
 
 # This section declares type mapping between the GraphQL and go type systems
 #

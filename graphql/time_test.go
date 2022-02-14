9a0650b27c0e1fdc48354bc7dd1f73473df758ef
Alessandro (Ale) Segala <43508+ItalyPaleAle@users.noreply.github.com>
2022-02-14 06:02:33 +0000

    Allow other formats when unmarshalling time

--- graphql/time_test.go
+++ graphql/time_test.go
@@ -23,3 +23,67 @@ func TestTime(t *testing.T) {
 		require.True(t, initialTime.Equal(newTime), "expected times %v and %v to equal", initialTime, newTime)
 	})
 }
+
+func TestUnmarshalTime(t *testing.T) {
+	tests := []struct {
+		name    string
+		in      interface{}
+		want    time.Time
+		wantErr bool
+	}{
+		{
+			name:    "RFC3339Nano",
+			in:      "2022-02-10T10:20:30.123456789Z",
+			want:    time.Date(2022, 02, 10, 10, 20, 30, 123456789, time.UTC),
+			wantErr: false,
+		},
+		{
+			name:    "RFC3339",
+			in:      "2022-02-10T10:20:30Z",
+			want:    time.Date(2022, 02, 10, 10, 20, 30, 0, time.UTC),
+			wantErr: false,
+		},
+		{
+			name:    "UTC ISO with time",
+			in:      "2022-02-10 10:20:30",
+			want:    time.Date(2022, 02, 10, 10, 20, 30, 0, time.UTC),
+			wantErr: false,
+		},
+		{
+			name:    "UTC ISO with time and nsec",
+			in:      "2022-02-10 10:20:30.123456789",
+			want:    time.Date(2022, 02, 10, 10, 20, 30, 123456789, time.UTC),
+			wantErr: false,
+		},
+		{
+			name:    "UTC ISO date",
+			in:      "2022-02-10",
+			want:    time.Date(2022, 02, 10, 0, 0, 0, 0, time.UTC),
+			wantErr: false,
+		},
+		{
+			name:    "Invalid format 1",
+			in:      "20220210",
+			want:    time.Time{},
+			wantErr: true,
+		},
+		{
+			name:    "Invalid format 1",
+			in:      "2022-02-33",
+			want:    time.Time{},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := UnmarshalTime(tt.in)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("UnmarshalTime() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !got.Equal(tt.want) {
+				t.Errorf("UnmarshalTime() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}

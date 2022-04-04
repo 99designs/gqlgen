596b8a0e4b52f150a5822ceac1a250d494007c71
morikuni <taihei.with+git@gmail.com>
2022-04-04 23:49:22 +0900

    Add AllowedMethods field to transport.Options
    to enable users to specify allowed HTTP methods.

--- graphql/handler/transport/options_test.go
+++ graphql/handler/transport/options_test.go
@@ -4,22 +4,34 @@ import (
 	"net/http"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/99designs/gqlgen/graphql/handler/testserver"
 	"github.com/99designs/gqlgen/graphql/handler/transport"
-	"github.com/stretchr/testify/assert"
 )
 
 func TestOptions(t *testing.T) {
-	h := testserver.New()
-	h.AddTransport(transport.Options{})
-
-	t.Run("responds to options requests", func(t *testing.T) {
+	t.Run("responds to options requests with default methods", func(t *testing.T) {
+		h := testserver.New()
+		h.AddTransport(transport.Options{})
 		resp := doRequest(h, "OPTIONS", "/graphql?query={me{name}}", ``)
 		assert.Equal(t, http.StatusOK, resp.Code)
 		assert.Equal(t, "OPTIONS, GET, POST", resp.Header().Get("Allow"))
 	})
 
+	t.Run("responds to options requests with specified methods", func(t *testing.T) {
+		h := testserver.New()
+		h.AddTransport(transport.Options{
+			AllowedMethods: []string{http.MethodOptions, http.MethodPost, http.MethodHead},
+		})
+		resp := doRequest(h, "OPTIONS", "/graphql?query={me{name}}", ``)
+		assert.Equal(t, http.StatusOK, resp.Code)
+		assert.Equal(t, "OPTIONS, POST, HEAD", resp.Header().Get("Allow"))
+	})
+
 	t.Run("responds to head requests", func(t *testing.T) {
+		h := testserver.New()
+		h.AddTransport(transport.Options{})
 		resp := doRequest(h, "HEAD", "/graphql?query={me{name}}", ``)
 		assert.Equal(t, http.StatusMethodNotAllowed, resp.Code)
 	})

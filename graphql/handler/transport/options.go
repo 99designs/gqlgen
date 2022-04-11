035e1d6eeb81179ddec3d36d8776212d8fe35cd6
morikuni <taihei.with+github@gmail.com>
2022-04-11 20:12:38 +0900

    Add AllowedMethods field to transport.Options (#2080)
    * Add AllowedMethods field to transport.Options
    
    to enable users to specify allowed HTTP methods.
    
    * Update graphql/handler/transport/options.go
    
    Co-authored-by: Giau. Tran Minh <giau.tmg@gmail.com>
    
    Co-authored-by: Steve Coffman <StevenACoffman@users.noreply.github.com>
    Co-authored-by: Giau. Tran Minh <giau.tmg@gmail.com>

--- graphql/handler/transport/options.go
+++ graphql/handler/transport/options.go
@@ -2,12 +2,16 @@ package transport
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/99designs/gqlgen/graphql"
 )
 
 // Options responds to http OPTIONS and HEAD requests
-type Options struct{}
+type Options struct {
+	// AllowedMethods is a list of allowed HTTP methods.
+	AllowedMethods []string
+}
 
 var _ graphql.Transport = Options{}
 
@@ -18,9 +22,16 @@ func (o Options) Supports(r *http.Request) bool {
 func (o Options) Do(w http.ResponseWriter, r *http.Request, exec graphql.GraphExecutor) {
 	switch r.Method {
 	case http.MethodOptions:
-		w.Header().Set("Allow", "OPTIONS, GET, POST")
+		w.Header().Set("Allow", o.allowedMethods())
 		w.WriteHeader(http.StatusOK)
 	case http.MethodHead:
 		w.WriteHeader(http.StatusMethodNotAllowed)
 	}
+}
+
+func (o Options) allowedMethods() string {
+	if len(o.AllowedMethods) == 0 {
+		return "OPTIONS, GET, POST"
+	}
+	return strings.Join(o.AllowedMethods, ", ")
 }

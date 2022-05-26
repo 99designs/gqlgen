afa6564989cd09dc2743848377970354af5dd422
MoofMonkey <11695747+MoofMonkey@users.noreply.github.com>
2022-05-26 07:06:54 +0300

    Added goroutine leak test for chat example

--- _examples/chat/chat_test.go
+++ _examples/chat/chat_test.go
@@ -1,6 +1,9 @@
 package chat
 
 import (
+	"fmt"
+	"runtime"
+	"sync"
 	"testing"
 	"time"
 
@@ -13,37 +16,59 @@ import (
 func TestChatSubscriptions(t *testing.T) {
 	c := client.New(handler.NewDefaultServer(NewExecutableSchema(New())))
 
-	sub := c.Websocket(`subscription @user(username:"vektah") { messageAdded(roomName:"#gophers") { text createdBy } }`)
-	defer sub.Close()
+	const batchSize = 16
+	var wg sync.WaitGroup
+	for i := 0; i < 1024; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			sub := c.Websocket(fmt.Sprintf(
+				`subscription @user(username:"vektah") { messageAdded(roomName:"#gophers%d") { text createdBy } }`,
+				i,
+			))
+			defer sub.Close()
 
-	go func() {
-		var resp interface{}
-		time.Sleep(10 * time.Millisecond)
-		err := c.Post(`mutation { 
-				a:post(text:"Hello!", roomName:"#gophers", username:"vektah") { id } 
-				b:post(text:"Hello Vektah!", roomName:"#gophers", username:"andrey") { id } 
-				c:post(text:"Whats up?", roomName:"#gophers", username:"vektah") { id } 
-			}`, &resp)
-		assert.NoError(t, err)
-	}()
+			go func() {
+				var resp interface{}
+				time.Sleep(10 * time.Millisecond)
+				err := c.Post(fmt.Sprintf(`mutation {
+					a:post(text:"Hello!", roomName:"#gophers%d", username:"vektah") { id }
+					b:post(text:"Hello Vektah!", roomName:"#gophers%d", username:"andrey") { id }
+					c:post(text:"Whats up?", roomName:"#gophers%d", username:"vektah") { id }
+				}`, i, i, i), &resp)
+				assert.NoError(t, err)
+			}()
 
-	var msg struct {
-		resp struct {
-			MessageAdded struct {
-				Text      string
-				CreatedBy string
+			var msg struct {
+				resp struct {
+					MessageAdded struct {
+						Text      string
+						CreatedBy string
+					}
+				}
+				err error
 			}
+
+			msg.err = sub.Next(&msg.resp)
+			require.NoError(t, msg.err, "sub.Next")
+			require.Equal(t, "Hello!", msg.resp.MessageAdded.Text)
+			require.Equal(t, "vektah", msg.resp.MessageAdded.CreatedBy)
+
+			msg.err = sub.Next(&msg.resp)
+			require.NoError(t, msg.err, "sub.Next")
+			require.Equal(t, "Whats up?", msg.resp.MessageAdded.Text)
+			require.Equal(t, "vektah", msg.resp.MessageAdded.CreatedBy)
+		}(i)
+		// wait for goroutines to finish every N tests to not get unhandled keepalives while starving on CPU
+		// TODO: add proper handling of keepalives to properly test concurrency?
+		if i%batchSize == 0 {
+			wg.Wait()
 		}
-		err error
 	}
+	wg.Wait()
 
-	msg.err = sub.Next(&msg.resp)
-	require.NoError(t, msg.err, "sub.Next")
-	require.Equal(t, "Hello!", msg.resp.MessageAdded.Text)
-	require.Equal(t, "vektah", msg.resp.MessageAdded.CreatedBy)
-
-	msg.err = sub.Next(&msg.resp)
-	require.NoError(t, msg.err, "sub.Next")
-	require.Equal(t, "Whats up?", msg.resp.MessageAdded.Text)
-	require.Equal(t, "vektah", msg.resp.MessageAdded.CreatedBy)
+	// 1 for the main thread, 1 for the testing package and remainder is reserved for the HTTP server threads
+	// TODO: use something like runtime.Stack to filter out HTTP server threads,
+	// TODO: which is required for proper concurrency and leaks testing
+	require.Less(t, runtime.NumGoroutine(), 1+1+batchSize*2, "goroutine leak")
 }

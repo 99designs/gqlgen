07dab4fcdda3cfce1f175dc4ac31aca7111b59db
atzedus <atz@mail.ru>
2022-12-07 14:51:12 +0300

    Ability to return multiple errors from resolver.

--- graphql/context_operation.go
+++ graphql/context_operation.go
@@ -109,6 +109,13 @@ func (c *OperationContext) Errorf(ctx context.Context, format string, args ...in
 // Error sends an error to the client, passing it through the formatter.
 // Deprecated: use graphql.AddError(ctx, err) instead
 func (c *OperationContext) Error(ctx context.Context, err error) {
+	if errList, ok := err.(gqlerror.List); ok {
+		for _, e := range errList {
+			AddError(ctx, e)
+		}
+		return
+	}
+	
 	AddError(ctx, err)
 }
 

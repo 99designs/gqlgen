f1648cdb60859c7e820a9c466c2f0bef34b1d290
Steve Coffman <steve@khanacademy.org>
2023-01-13 16:11:34 -0500

    fmt and regenerate
    Signed-off-by: Steve Coffman <steve@khanacademy.org>

--- graphql/handler/transport/websocket_resolver_error.go
+++ graphql/handler/transport/websocket_resolver_error.go
@@ -20,33 +20,35 @@ type subscriptionError struct {
 
 // AddSubscriptionError is used to let websocket return an error message after subscription resolver returns a channel.
 // for example:
-// func (r *subscriptionResolver) Method(ctx context.Context) (<-chan *model.Message, error) {
-//	ch := make(chan *model.Message)
-//	go func() {
-//      defer func() {
-//			close(ch)
-//      }
-//		// some kind of block processing (e.g.: gRPC client streaming)
-//		stream, err := gRPCClientStreamRequest(ctx)
-//		if err != nil {
-//			   transport.AddSubscriptionError(ctx, err)
-//             return // must return and close channel so websocket can send error back
-//      }
-//		for {
-//			m, err := stream.Recv()
-//			if err == io.EOF {
-//				return
-//			}
-//			if err != nil {
-//			   transport.AddSubscriptionError(ctx, err)
-//             return // must return and close channel so websocket can send error back
-//			}
-//			ch <- m
-//		}
-//	}()
 //
-//	return ch, nil
-//}
+//	func (r *subscriptionResolver) Method(ctx context.Context) (<-chan *model.Message, error) {
+//		ch := make(chan *model.Message)
+//		go func() {
+//	     defer func() {
+//				close(ch)
+//	     }
+//			// some kind of block processing (e.g.: gRPC client streaming)
+//			stream, err := gRPCClientStreamRequest(ctx)
+//			if err != nil {
+//				   transport.AddSubscriptionError(ctx, err)
+//	            return // must return and close channel so websocket can send error back
+//	     }
+//			for {
+//				m, err := stream.Recv()
+//				if err == io.EOF {
+//					return
+//				}
+//				if err != nil {
+//				   transport.AddSubscriptionError(ctx, err)
+//	            return // must return and close channel so websocket can send error back
+//				}
+//				ch <- m
+//			}
+//		}()
+//
+//		return ch, nil
+//	}
+//
 // see https://github.com/99designs/gqlgen/pull/2506 for more details
 func AddSubscriptionError(ctx context.Context, err *gqlerror.Error) {
 	subscriptionErrStruct := getSubscriptionErrorStruct(ctx)

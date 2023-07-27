99designs/gqlgen

commit 34bbc450c502919cd46c5eefcc66341ef697c0e8

Use the go:embed API to lookup templates (#2262)
* Switch the templates package internally to read from TemplateFS

Users are expected to pass in the FS by using the embed API.

* Update all usages of templates.Render to use the TemplateFS option

* Fix unit tests

* Fix linter error

* Commit generated changes

Doesn't look like anything has changed though. Maybe just a different
whitespace character.

* Fix test

codegen/templates/templates.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"fmt"
 	"go/types"
+	"io/fs"
 	"os"
 	"path/filepath"
 	"reflect"
@@ -35,6 +36,11 @@ type Options struct {
 	// the plugin processor will look for .gotpl files
 	// in the same directory of where you wrote the plugin.
 	Template string
+
+	// Use the go:embed API to collect all the template files you want to pass into Render
+	// this is an alternative to passing the Template option
+	TemplateFS fs.FS
+
 	// Filename is the name of the file that will be
 	// written to the system disk once the template is rendered.
 	Filename        string
@@ -62,55 +68,27 @@ func Render(cfg Options) error {
 	}
 	CurrentImports = &Imports{packages: cfg.Packages, destDir: filepath.Dir(cfg.Filename)}
 
-	// load path relative to calling source file
-	_, callerFile, _, _ := runtime.Caller(1)
-	rootDir := filepath.Dir(callerFile)
-
 	funcs := Funcs()
 	for n, f := range cfg.Funcs {
 		funcs[n] = f
 	}
+
 	t := template.New("").Funcs(funcs)
+	t, err := parseTemplates(cfg, t)
+	if err != nil {
+		return err
+	}
 
-	var roots []string
-	if cfg.Template != "" {
-		var err error
-		t, err = t.New("template.gotpl").Parse(cfg.Template)
-		if err != nil {
-			return fmt.Errorf("error with provided template: %w", err)
+	roots := make([]string, 0, len(t.Templates()))
+	for _, template := range t.Templates() {
+		// templates that end with _.gotpl are special files we don't want to include
+		if strings.HasSuffix(template.Name(), "_.gotpl") ||
+			// filter out templates added with {{ template xxx }} syntax inside the template file
+			!strings.HasSuffix(template.Name(), ".gotpl") {
+			continue
 		}
-		roots = append(roots, "template.gotpl")
-	} else {
-		// load all the templates in the directory
-		err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			name := filepath.ToSlash(strings.TrimPrefix(path, rootDir+string(os.PathSeparator)))
-			if !strings.HasSuffix(info.Name(), ".gotpl") {
-				return nil
-			}
-			// omit any templates with "_" at the end of their name, which are meant for specific contexts only
-			if strings.HasSuffix(info.Name(), "_.gotpl") {
-				return nil
-			}
-			b, err := os.ReadFile(path)
-			if err != nil {
-				return err
-			}
 
-			t, err = t.New(name).Parse(string(b))
-			if err != nil {
-				return fmt.Errorf("%s: %w", cfg.Filename, err)
-			}
-
-			roots = append(roots, name)
-
-			return nil
-		})
-		if err != nil {
-			return fmt.Errorf("locating templates: %w", err)
-		}
+		roots = append(roots, template.Name())
 	}
 
 	// then execute all the important looking ones in order, adding them to the same file
@@ -124,6 +102,7 @@ func Render(cfg Options) error {
 		}
 		return roots[i] < roots[j]
 	})
+
 	var buf bytes.Buffer
 	for _, root := range roots {
 		if cfg.RegionTags {
@@ -155,7 +134,7 @@ func Render(cfg Options) error {
 	result.WriteString("import (\n")
 	result.WriteString(CurrentImports.String())
 	result.WriteString(")\n")
-	_, err := buf.WriteTo(&result)
+	_, err = buf.WriteTo(&result)
 	if err != nil {
 		return err
 	}
@@ -168,6 +147,34 @@ func Render(cfg Options) error {
 
 	cfg.Packages.Evict(code.ImportPathForDir(filepath.Dir(cfg.Filename)))
 	return nil
+}
+
+func parseTemplates(cfg Options, t *template.Template) (*template.Template, error) {
+	if cfg.Template != "" {
+		var err error
+		t, err = t.New("template.gotpl").Parse(cfg.Template)
+		if err != nil {
+			return nil, fmt.Errorf("error with provided template: %w", err)
+		}
+		return t, nil
+	}
+
+	var fileSystem fs.FS
+	if cfg.TemplateFS != nil {
+		fileSystem = cfg.TemplateFS
+	} else {
+		// load path relative to calling source file
+		_, callerFile, _, _ := runtime.Caller(1)
+		rootDir := filepath.Dir(callerFile)
+		fileSystem = os.DirFS(rootDir)
+	}
+
+	t, err := t.ParseFS(fileSystem, "*.gotpl")
+	if err != nil {
+		return nil, fmt.Errorf("locating templates: %w", err)
+	}
+
+	return t, nil
 }
 
 func center(width int, pad string, s string) string {

_examples/embedding/subdir/gendir/generated.go
@@ -178,7 +178,7 @@ var sources = []*ast.Source{
 	{Name: "../federation/directives.graphql", Input: `
 	scalar _Any
 	scalar _FieldSet
-	
+
 	directive @external on FIELD_DEFINITION
 	directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
 	directive @provides(fields: _FieldSet!) on FIELD_DEFINITION

_examples/embedding/subdir/root_.generated.go
@@ -172,7 +172,7 @@ var sources = []*ast.Source{
 	{Name: "federation/directives.graphql", Input: `
 	scalar _Any
 	scalar _FieldSet
-	
+
 	directive @external on FIELD_DEFINITION
 	directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
 	directive @provides(fields: _FieldSet!) on FIELD_DEFINITION

_examples/federation/accounts/graph/generated/generated.go
@@ -266,7 +266,7 @@ type User @key(fields: "id") {
 	{Name: "../../federation/directives.graphql", Input: `
 	scalar _Any
 	scalar _FieldSet
-	
+
 	directive @external on FIELD_DEFINITION
 	directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
 	directive @provides(fields: _FieldSet!) on FIELD_DEFINITION

_examples/federation/products/graph/generated/generated.go
@@ -294,7 +294,7 @@ type Product @key(fields: "manufacturer { id } id") @key(fields: "upc") {
 	{Name: "../../federation/directives.graphql", Input: `
 	scalar _Any
 	scalar _FieldSet
-	
+
 	directive @external on FIELD_DEFINITION
 	directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
 	directive @provides(fields: _FieldSet!) on FIELD_DEFINITION

_examples/federation/reviews/graph/generated/generated.go
@@ -326,7 +326,7 @@ extend type Product @key(fields: " manufacturer{  id} id") {
 	{Name: "../../federation/directives.graphql", Input: `
 	scalar _Any
 	scalar _FieldSet
-	
+
 	directive @external on FIELD_DEFINITION
 	directive @requires(fields: _FieldSet!) on FIELD_DEFINITION
 	directive @provides(fields: _FieldSet!) on FIELD_DEFINITION

codegen/generate.go
@@ -1,6 +1,7 @@
 package codegen
 
 import (
+	"embed"
 	"errors"
 	"fmt"
 	"os"
@@ -12,6 +13,9 @@ import (
 	"github.com/99designs/gqlgen/codegen/templates"
 	"github.com/vektah/gqlparser/v2/ast"
 )
+
+//go:embed *.gotpl
+var codegenTemplates embed.FS
 
 func GenerateCode(data *Data) error {
 	if !data.Config.Exec.IsDefined() {
@@ -36,6 +40,7 @@ func generateSingleFile(data *Data) error {
 		RegionTags:      true,
 		GeneratedHeader: true,
 		Packages:        data.Config.Packages,
+		TemplateFS:      codegenTemplates,
 	})
 }
 
@@ -82,6 +87,7 @@ func generatePerSchema(data *Data) error {
 			RegionTags:      true,
 			GeneratedHeader: true,
 			Packages:        data.Config.Packages,
+			TemplateFS:      codegenTemplates,
 		})
 		if err != nil {
 			return err
@@ -145,6 +151,7 @@ func generateRootFile(data *Data) error {
 		RegionTags:      false,
 		GeneratedHeader: true,
 		Packages:        data.Config.Packages,
+		TemplateFS:      codegenTemplates,
 	})
 }
 

codegen/templates/templates_test.go
@@ -1,13 +1,19 @@
 package templates
 
 import (
+	"embed"
 	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/99designs/gqlgen/internal/code"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+//go:embed *.gotpl
+var templateFS embed.FS
 
 func TestToGo(t *testing.T) {
 	require.Equal(t, "ToCamel", ToGo("TO_CAMEL"))
@@ -118,4 +124,31 @@ func TestTemplateOverride(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestRenderFS(t *testing.T) {
+
+	tempDir := t.TempDir()
+
+	outDir := filepath.Join(tempDir, "output")
+
+	_ = os.Mkdir(outDir, 0o755)
+
+	f, err := os.CreateTemp(outDir, "gqlgen.go")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer f.Close()
+	defer os.RemoveAll(f.Name())
+	err = Render(Options{TemplateFS: templateFS, Filename: f.Name(), Packages: &code.Packages{}})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectedString := "package \n\nimport (\n)\nthis is my test package"
+	actualContents, _ := os.ReadFile(f.Name())
+	actualContentsStr := string(actualContents)
+
+	// don't look at last character since it's \n on Linux and \r\n on Windows
+	assert.Equal(t, expectedString, actualContentsStr[:len(expectedString)])
 }

commit f2b168ed33d6eddf5aa18d2c6ef48898f44a2bcf

Fix plugin template resolution
- According to the documentation comment for [templates.Options], if the
  `Template` and `TemplateFS` fields are empty, it `Render` should find
  the `.gotpl` files from the calling plugin. However, it looks like
  this is broken since #2262 moved the resolution code to a separate
  helper function. This results in broken behavior in consumers such as
  [infiotinc/gqlgenc](https://github.com/infiotinc/gqlgenc) when they
  use the latest version of `gqlgen` as instead of finding the template
  from the plugin, the test template from this package is used which
  outputs only: `this is my test package`.
- The cause for this is that `runtime.Caller` was still only skipping
  one stack level which means that it was finding the `Render` function
  instead of its caller.

[templates.Options]: https://pkg.go.dev/github.com/99designs/gqlgen@v0.17.35/codegen/templates#Options

codegen/templates/templates.go
@@ -172,7 +172,7 @@ func parseTemplates(cfg Options, t *template.Template) (*template.Template, erro
 		fileSystem = cfg.TemplateFS
 	} else {
 		// load path relative to calling source file
-		_, callerFile, _, _ := runtime.Caller(1)
+		_, callerFile, _, _ := runtime.Caller(2)
 		rootDir := filepath.Dir(callerFile)
 		fileSystem = os.DirFS(rootDir)
 	}
9267b8d7d00cc68b07ae7b372f49aae0a4ac880a
Giau. Tran Minh <hello@giautm.dev>
2023-09-08 17:00:19 +0700

    chore: more comment

--- internal/code/testdata/p/p.go
+++ internal/code/testdata/p/p.go
@@ -1,6 +1,9 @@
 //go:build private
 // +build private
 
+// This file is excluded from the build unless the "private" build tag is set.
+// This is used to test loading private packages.
+// See internal/code/packages_test.go for more details.
 package p
 
 import (

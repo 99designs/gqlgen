5b65689140e4a688f4b49827785bc94044ac6693
Ben Peters <bpeters@alumni.cmu.edu>
2024-01-10 15:50:39 -0600

    Fix code generation for federated multi-key, multi-entity types (#2877)
    * fix codegen for multi-key, multi-entity type
    
    * adding tests
    
    * updated generated code
    
    ---------
    
    Co-authored-by: Ben Peters <bpeters@octolabs.io>

--- plugin/federation/federation_test.go
+++ plugin/federation/federation_test.go
@@ -12,22 +12,24 @@ import (
 func TestWithEntities(t *testing.T) {
 	f, cfg := load(t, "testdata/allthethings/gqlgen.yml")
 
-	require.Equal(t, []string{"ExternalExtension", "Hello", "MoreNesting", "NestedKey", "VeryNestedKey", "World"}, cfg.Schema.Types["_Entity"].Types)
+	require.Equal(t, []string{"ExternalExtension", "Hello", "MoreNesting", "MultiHelloMultiKey", "NestedKey", "VeryNestedKey", "World"}, cfg.Schema.Types["_Entity"].Types)
 
-	require.Len(t, cfg.Schema.Types["Entity"].Fields, 6)
+	require.Len(t, cfg.Schema.Types["Entity"].Fields, 8)
 
 	require.Equal(t, "findExternalExtensionByUpc", cfg.Schema.Types["Entity"].Fields[0].Name)
 	require.Equal(t, "findHelloByName", cfg.Schema.Types["Entity"].Fields[1].Name)
 	// missing on purpose: all @external fields:
 	// require.Equal(t, "findMoreNestingByID", cfg.Schema.Types["Entity"].Fields[2].Name)
-	require.Equal(t, "findNestedKeyByIDAndHelloName", cfg.Schema.Types["Entity"].Fields[2].Name)
-	require.Equal(t, "findVeryNestedKeyByIDAndHelloNameAndWorldFooAndWorldBarAndMoreWorldFoo", cfg.Schema.Types["Entity"].Fields[3].Name)
-	require.Equal(t, "findWorldByFoo", cfg.Schema.Types["Entity"].Fields[4].Name)
-	require.Equal(t, "findWorldByBar", cfg.Schema.Types["Entity"].Fields[5].Name)
+	require.Equal(t, "findManyMultiHelloMultiKeyByNames", cfg.Schema.Types["Entity"].Fields[2].Name)
+	require.Equal(t, "findManyMultiHelloMultiKeyByKey2s", cfg.Schema.Types["Entity"].Fields[3].Name)
+	require.Equal(t, "findNestedKeyByIDAndHelloName", cfg.Schema.Types["Entity"].Fields[4].Name)
+	require.Equal(t, "findVeryNestedKeyByIDAndHelloNameAndWorldFooAndWorldBarAndMoreWorldFoo", cfg.Schema.Types["Entity"].Fields[5].Name)
+	require.Equal(t, "findWorldByFoo", cfg.Schema.Types["Entity"].Fields[6].Name)
+	require.Equal(t, "findWorldByBar", cfg.Schema.Types["Entity"].Fields[7].Name)
 
 	require.NoError(t, f.MutateConfig(cfg))
 
-	require.Len(t, f.Entities, 6)
+	require.Len(t, f.Entities, 7)
 
 	require.Equal(t, "ExternalExtension", f.Entities[0].Name)
 	require.Len(t, f.Entities[0].Resolvers, 1)
@@ -44,40 +46,49 @@ func TestWithEntities(t *testing.T) {
 	require.Equal(t, "MoreNesting", f.Entities[2].Name)
 	require.Len(t, f.Entities[2].Resolvers, 0)
 
-	require.Equal(t, "NestedKey", f.Entities[3].Name)
-	require.Len(t, f.Entities[3].Resolvers, 1)
-	require.Len(t, f.Entities[3].Resolvers[0].KeyFields, 2)
-	require.Equal(t, "id", f.Entities[3].Resolvers[0].KeyFields[0].Definition.Name)
+	require.Equal(t, "MultiHelloMultiKey", f.Entities[3].Name)
+	require.Len(t, f.Entities[3].Resolvers, 2)
+	require.Len(t, f.Entities[3].Resolvers[0].KeyFields, 1)
+	require.Len(t, f.Entities[3].Resolvers[1].KeyFields, 1)
+	require.Equal(t, "name", f.Entities[3].Resolvers[0].KeyFields[0].Definition.Name)
 	require.Equal(t, "String", f.Entities[3].Resolvers[0].KeyFields[0].Definition.Type.Name())
-	require.Equal(t, "helloName", f.Entities[3].Resolvers[0].KeyFields[1].Definition.Name)
-	require.Equal(t, "String", f.Entities[3].Resolvers[0].KeyFields[1].Definition.Type.Name())
+	require.Equal(t, "key2", f.Entities[3].Resolvers[1].KeyFields[0].Definition.Name)
+	require.Equal(t, "String", f.Entities[3].Resolvers[1].KeyFields[0].Definition.Type.Name())
 
-	require.Equal(t, "VeryNestedKey", f.Entities[4].Name)
+	require.Equal(t, "NestedKey", f.Entities[4].Name)
 	require.Len(t, f.Entities[4].Resolvers, 1)
-	require.Len(t, f.Entities[4].Resolvers[0].KeyFields, 5)
+	require.Len(t, f.Entities[4].Resolvers[0].KeyFields, 2)
 	require.Equal(t, "id", f.Entities[4].Resolvers[0].KeyFields[0].Definition.Name)
 	require.Equal(t, "String", f.Entities[4].Resolvers[0].KeyFields[0].Definition.Type.Name())
 	require.Equal(t, "helloName", f.Entities[4].Resolvers[0].KeyFields[1].Definition.Name)
 	require.Equal(t, "String", f.Entities[4].Resolvers[0].KeyFields[1].Definition.Type.Name())
-	require.Equal(t, "worldFoo", f.Entities[4].Resolvers[0].KeyFields[2].Definition.Name)
-	require.Equal(t, "String", f.Entities[4].Resolvers[0].KeyFields[2].Definition.Type.Name())
-	require.Equal(t, "worldBar", f.Entities[4].Resolvers[0].KeyFields[3].Definition.Name)
-	require.Equal(t, "Int", f.Entities[4].Resolvers[0].KeyFields[3].Definition.Type.Name())
-	require.Equal(t, "moreWorldFoo", f.Entities[4].Resolvers[0].KeyFields[4].Definition.Name)
-	require.Equal(t, "String", f.Entities[4].Resolvers[0].KeyFields[4].Definition.Type.Name())
 
-	require.Len(t, f.Entities[4].Requires, 2)
-	require.Equal(t, f.Entities[4].Requires[0].Name, "id")
-	require.Equal(t, f.Entities[4].Requires[1].Name, "helloSecondary")
-
-	require.Equal(t, "World", f.Entities[5].Name)
-	require.Len(t, f.Entities[5].Resolvers, 2)
-	require.Len(t, f.Entities[5].Resolvers[0].KeyFields, 1)
-	require.Equal(t, "foo", f.Entities[5].Resolvers[0].KeyFields[0].Definition.Name)
+	require.Equal(t, "VeryNestedKey", f.Entities[5].Name)
+	require.Len(t, f.Entities[5].Resolvers, 1)
+	require.Len(t, f.Entities[5].Resolvers[0].KeyFields, 5)
+	require.Equal(t, "id", f.Entities[5].Resolvers[0].KeyFields[0].Definition.Name)
 	require.Equal(t, "String", f.Entities[5].Resolvers[0].KeyFields[0].Definition.Type.Name())
-	require.Len(t, f.Entities[5].Resolvers[1].KeyFields, 1)
-	require.Equal(t, "bar", f.Entities[5].Resolvers[1].KeyFields[0].Definition.Name)
-	require.Equal(t, "Int", f.Entities[5].Resolvers[1].KeyFields[0].Definition.Type.Name())
+	require.Equal(t, "helloName", f.Entities[5].Resolvers[0].KeyFields[1].Definition.Name)
+	require.Equal(t, "String", f.Entities[5].Resolvers[0].KeyFields[1].Definition.Type.Name())
+	require.Equal(t, "worldFoo", f.Entities[5].Resolvers[0].KeyFields[2].Definition.Name)
+	require.Equal(t, "String", f.Entities[5].Resolvers[0].KeyFields[2].Definition.Type.Name())
+	require.Equal(t, "worldBar", f.Entities[5].Resolvers[0].KeyFields[3].Definition.Name)
+	require.Equal(t, "Int", f.Entities[5].Resolvers[0].KeyFields[3].Definition.Type.Name())
+	require.Equal(t, "moreWorldFoo", f.Entities[5].Resolvers[0].KeyFields[4].Definition.Name)
+	require.Equal(t, "String", f.Entities[5].Resolvers[0].KeyFields[4].Definition.Type.Name())
+
+	require.Len(t, f.Entities[5].Requires, 2)
+	require.Equal(t, f.Entities[5].Requires[0].Name, "id")
+	require.Equal(t, f.Entities[5].Requires[1].Name, "helloSecondary")
+
+	require.Equal(t, "World", f.Entities[6].Name)
+	require.Len(t, f.Entities[6].Resolvers, 2)
+	require.Len(t, f.Entities[6].Resolvers[0].KeyFields, 1)
+	require.Equal(t, "foo", f.Entities[6].Resolvers[0].KeyFields[0].Definition.Name)
+	require.Equal(t, "String", f.Entities[6].Resolvers[0].KeyFields[0].Definition.Type.Name())
+	require.Len(t, f.Entities[6].Resolvers[1].KeyFields, 1)
+	require.Equal(t, "bar", f.Entities[6].Resolvers[1].KeyFields[0].Definition.Name)
+	require.Equal(t, "Int", f.Entities[6].Resolvers[1].KeyFields[0].Definition.Type.Name())
 }
 
 func TestNoEntities(t *testing.T) {
@@ -124,8 +135,8 @@ func TestEntityInterfaces(t *testing.T) {
 func TestCodeGeneration(t *testing.T) {
 	f, cfg := load(t, "testdata/allthethings/gqlgen.yml")
 
-	require.Len(t, cfg.Schema.Types["_Entity"].Types, 6)
-	require.Len(t, f.Entities, 6)
+	require.Len(t, cfg.Schema.Types["_Entity"].Types, 7)
+	require.Len(t, f.Entities, 7)
 
 	require.NoError(t, f.MutateConfig(cfg))
 

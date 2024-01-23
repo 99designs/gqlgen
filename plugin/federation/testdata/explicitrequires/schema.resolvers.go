e137fec3c89b05c9a1d8cd656ceeda16685cfc05
Jesse Lovelace <jesse.lovelace@gmail.com>
2024-01-23 11:32:44 -0500

    Adding explicit requires testsing and make requires follow federation package.

---
+++ plugin/federation/testdata/explicitrequires/schema.resolvers.go
@@ -0,0 +1,4 @@
+package explicitrequires
+
+// This file will be automatically regenerated based on the schema, any resolver implementations
+// will be copied through when generating and any unknown code will be moved to the end.

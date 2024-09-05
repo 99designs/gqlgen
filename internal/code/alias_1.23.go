7797e91c74c3abdb799d9f5dd42bcdfde9e52e32
Giau. Tran Minh <hello@giautm.dev>
2024-09-06 00:27:00 +0700

    code: `Unalias` element of pointer

--- internal/code/alias_1.23.go
+++ internal/code/alias_1.23.go
@@ -8,5 +8,8 @@ import (
 
 // Unalias unwraps an alias type
 func Unalias(t types.Type) types.Type {
+	if p, ok := t.(*types.Pointer); ok {
+		return types.NewPointer(Unalias(p.Elem()))
+	}
 	return types.Unalias(t)
 }

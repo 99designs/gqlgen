6b9e40e83e942611622f4fad4baa06af521780bd
Ly (Haily) Nguyen <67850771+hailyngx@users.noreply.github.com>
2024-09-12 18:52:07 -0700

    make rewrite default for resolver layout single-file (#3243)

--- codegen/testserver/nullabledirectives/generated/resolvers/resolver.go
+++ codegen/testserver/nullabledirectives/generated/resolvers/resolver.go
@@ -1,6 +1,6 @@
 package resolver
 
-// THIS CODE IS A STARTING POINT ONLY. IT WILL NOT BE UPDATED WITH SCHEMA CHANGES.
+// THIS CODE WILL BE UPDATED WITH SCHEMA CHANGES. PREVIOUS IMPLEMENTATION FOR SCHEMA CHANGES WILL BE KEPT IN THE COMMENT SECTION. IMPLEMENTATION FOR UNCHANGED SCHEMA WILL BE KEPT.
 
 import (
 	"context"
@@ -19,3 +19,13 @@ func (r *queryResolver) DirectiveSingleNullableArg(ctx context.Context, arg1 *st
 func (r *Resolver) Query() nullabledirectives.QueryResolver { return &queryResolver{r} }
 
 type queryResolver struct{ *Resolver }
+
+// !!! WARNING !!!
+// The code below was going to be deleted when updating resolvers. It has been copied here so you have
+// one last chance to move it out of harms way if you want. There are two reasons this happens:
+//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
+//    it when you're done.
+//  - You have helper methods in this file. Move them out to keep these resolver files clean.
+/*
+	type Resolver struct{}
+*/

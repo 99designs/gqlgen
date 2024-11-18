89c4e8436357ffd8056eb1172d7535ab99f24d62
Shinra Yamakawa <13755428+OldBigBuddha@users.noreply.github.com>
2024-11-18 20:47:43 +0900

    add `worker_limit` option for server code generation (#3376)
    * add property in exec
    
    * add semaphore for goroutine limitation in template
    
    * generation test with worker_limit option
    
    * add comment
    
    * update init-template
    
    * misc

--- api/generate_test.go
+++ api/generate_test.go
@@ -34,6 +34,10 @@ func TestGenerate(t *testing.T) {
 			name:    "federation2",
 			workDir: filepath.Join(wd, "testdata", "federation2"),
 		},
+		{
+			name:    "worker_limit",
+			workDir: filepath.Join(wd, "testdata", "workerlimit"),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {

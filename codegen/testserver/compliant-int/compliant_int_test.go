e76bef0fd9ce4c44bc240563cc8775f7cb376e9d
Philip Hughes <phughes@secureworks.com>
2024-12-03 15:14:09 -0500

    add int input integration tests

--- codegen/testserver/compliant-int/compliant_int_test.go
+++ codegen/testserver/compliant-int/compliant_int_test.go
@@ -1,11 +1,12 @@
-//go:generate go run ../../../testdata/gqlgen.go -config gqlgen_default.yml
-//go:generate go run ../../../testdata/gqlgen.go -config gqlgen_compliant.yml
-//go:generate go run ../../../testdata/gqlgen.go -config gqlgen_compliant_input_int.yml
+//go:generate go run ../../../testdata/gqlgen.go -config gqlgen_default.yml -stub generated_default/stub.go
+//go:generate go run ../../../testdata/gqlgen.go -config gqlgen_compliant.yml -stub generated_compliant/stub.go
+//go:generate go run ../../../testdata/gqlgen.go -config gqlgen_compliant_input_int.yml -stub generated_compliant_input_int/stub.go
 
 package compliant_int
 
 import (
 	"bytes"
+	"context"
 	"fmt"
 	"go/ast"
 	"go/format"
@@ -16,6 +17,12 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/99designs/gqlgen/client"
+	genstrict "github.com/99designs/gqlgen/codegen/testserver/compliant-int/generated_compliant"
+	gendefault "github.com/99designs/gqlgen/codegen/testserver/compliant-int/generated_default"
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/stretchr/testify/require"
 )
 
@@ -41,7 +48,7 @@ func TestCodegen(t *testing.T) {
 			},
 		},
 		{
-			name:    "compliant model configuration in yaml",
+			name:    "strict compliant model configuration in yaml",
 			pkgPath: "generated_compliant",
 			signature: map[string]string{
 				"EchoIntToInt":     "func(ctx context.Context, n *int32) (int32, error)",
@@ -118,6 +125,73 @@ func TestCodegen(t *testing.T) {
 			t.Run("models", func(t *testing.T) {
 				require.Equal(t, tc.models, modelsMap)
 			})
+		})
+	}
+}
+
+func TestIntegration(t *testing.T) {
+	defaultStub := &gendefault.Stub{}
+	defaultStub.QueryResolver.EchoIntToInt = func(_ context.Context, n *int) (int, error) {
+		if n == nil {
+			return 0, nil
+		}
+		return *n, nil
+	}
+	// compliantStub := &gencompliant.Stub{}
+	// compliantStub.QueryResolver.EchoIntToInt = func(_ context.Context, n *int) (int32, error) {
+	// 	if n == nil {
+	// 		return 0, nil
+	// 	}
+	// 	return int32(*n), nil
+	// }
+	strictStub := &genstrict.Stub{}
+	strictStub.QueryResolver.EchoIntToInt = func(_ context.Context, n *int32) (int32, error) {
+		if n == nil {
+			return 0, nil
+		}
+		return *n, nil
+	}
+
+	cases := []struct {
+		name      string
+		exec      graphql.ExecutableSchema
+		willError bool
+	}{
+		{
+			name:      "default generation allows int32 overflow inputs",
+			exec:      gendefault.NewExecutableSchema(gendefault.Config{Resolvers: defaultStub}),
+			willError: false,
+		},
+		// {
+		// 	// rely on client to send good data, but the server should not error
+		// 	name:      "compliant generation with int input setting allows int32 overflow inputs",
+		// 	exec:      gencompliant.NewExecutableSchema(gencompliant.Config{Resolvers: compliantStub}),
+		// 	willError: false,
+		// },
+		{
+			name:      "strict compliant generation does not allow int32 overflow inputs",
+			exec:      genstrict.NewExecutableSchema(genstrict.Config{Resolvers: strictStub}),
+			willError: true,
+		},
+	}
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			srv := handler.New(tc.exec)
+			srv.AddTransport(transport.POST{})
+
+			c := client.New(srv)
+
+			var resp struct {
+				EchoIntToInt int
+			}
+			err := c.Post(`query { echoIntToInt(n: 2147483648) }`, &resp)
+			if tc.willError {
+				require.EqualError(t, err, `[{"message":"2147483648 overflows 32-bit integer","path":["echoIntToInt","n"]}]`)
+				require.Equal(t, 0, resp.EchoIntToInt)
+				return
+			}
+			require.NoError(t, err)
+			require.Equal(t, 2147483648, resp.EchoIntToInt)
 		})
 	}
 }

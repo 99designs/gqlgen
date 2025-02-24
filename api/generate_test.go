5310d084cc4c112fc8c9cb89a4bd10156a560c88
Islam Aliev <aliev.islam@gmail.com>
2025-02-25 00:15:37 +0100

    feat: Add SchemaMutator method to be used by plugins (#3554)
    * Add SchemaMutator method to be used by plugins
    
    * Fix lint

--- api/generate_test.go
+++ api/generate_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
 	"testing"
@@ -8,6 +9,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"github.com/99designs/gqlgen/codegen/config"
+	"github.com/vektah/gqlparser/v2/ast"
 )
 
 func cleanup(workDir string) {
@@ -51,6 +53,80 @@ func TestGenerate(t *testing.T) {
 			require.NoError(t, err, "failed to load config")
 			err = Generate(cfg)
 			require.NoError(t, err, "failed to generate code")
+		})
+	}
+}
+
+type testSchemaMutator struct {
+	name        string
+	shouldError bool
+}
+
+func (t *testSchemaMutator) Name() string {
+	return t.name
+}
+
+func (t *testSchemaMutator) MutateSchema(schema *ast.Schema) error {
+	if t.shouldError {
+		return errors.New("deliberate schema mutation error")
+	}
+	schema.Types["TestType"] = &ast.Definition{
+		Kind: ast.Object,
+		Name: "TestType",
+		Fields: ast.FieldList{
+			{
+				Name: "id",
+				Type: ast.NamedType("ID", nil),
+			},
+		},
+	}
+	return nil
+}
+
+func TestGenerateWithSchemaMutator(t *testing.T) {
+	wd, err := os.Getwd()
+	require.NoError(t, err)
+
+	tests := []struct {
+		name        string
+		mutator     *testSchemaMutator
+		shouldError bool
+	}{
+		{
+			name:        "successful schema mutation",
+			mutator:     &testSchemaMutator{name: "test-mutator", shouldError: false},
+			shouldError: false,
+		},
+		{
+			name:        "failed schema mutation",
+			mutator:     &testSchemaMutator{name: "error-mutator", shouldError: true},
+			shouldError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			workDir := filepath.Join(wd, "testdata", "default")
+			t.Cleanup(func() {
+				cleanup(workDir)
+				_ = os.Chdir(wd)
+			})
+
+			err := os.Chdir(workDir)
+			require.NoError(t, err)
+
+			cfg, err := config.LoadConfigFromDefaultLocations()
+			require.NoError(t, err)
+
+			err = Generate(cfg, AddPlugin(tt.mutator))
+			if tt.shouldError {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), "deliberate schema mutation error")
+			} else {
+				require.NoError(t, err)
+				require.Contains(t, cfg.Schema.Types, "TestType")
+				require.Equal(t, ast.Object, cfg.Schema.Types["TestType"].Kind)
+			}
 		})
 	}
 }

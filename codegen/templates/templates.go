8890a080a539375e6517f15228f478e440f8a199
Steve Coffman <steve@khanacademy.org>
2025-04-23 11:09:03 -0400

    Apply band-aid
    Signed-off-by: Steve Coffman <steve@khanacademy.org>

--- codegen/templates/templates.go
+++ codegen/templates/templates.go
@@ -256,8 +256,12 @@ func ref(p types.Type) string {
 	typeString := CurrentImports.LookupType(p)
 	// TODO(steve): figure out why this is needed
 	// otherwise inconsistent sometimes
+	// see https://github.com/99designs/gqlgen/issues/3414#issuecomment-2822856422
 	if typeString == "interface{}" {
 		return "any"
+	}
+	if typeString == "map[string]interface{}" {
+		return "map[string]any"
 	}
 	return typeString
 }

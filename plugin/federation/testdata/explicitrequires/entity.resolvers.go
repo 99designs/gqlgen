60983092836c8f4f35a916ec736cc0c123029a62
Parker Roan <parker@shipt.com>
2025-06-24 11:05:25 -0500

    go generate commit

--- plugin/federation/testdata/explicitrequires/entity.resolvers.go
+++ plugin/federation/testdata/explicitrequires/entity.resolvers.go
@@ -185,15 +185,3 @@ func (r *entityResolver) FindWorldWithMultipleKeysByBar(ctx context.Context, bar
 func (r *Resolver) Entity() generated.EntityResolver { return &entityResolver{r} }
 
 type entityResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	func (r *entityResolver) FindManyMultiPlanetRequiresNestedObjectArrayByNames(ctx context.Context, reps []*generated.MultiPlanetRequiresNestedObjectArrayByNamesInput) ([]*generated.MultiPlanetRequiresNestedObjectArray, error) {
-	panic(fmt.Errorf("not implemented: FindManyMultiPlanetRequiresNestedObjectArrayByNames - findManyMultiPlanetRequiresNestedObjectArrayByNames"))
-}
-*/

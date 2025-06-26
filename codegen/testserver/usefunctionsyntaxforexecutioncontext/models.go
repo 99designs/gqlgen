468603b2c2ec34f35fe0a6c22ec96b893d25641e
Adam Gonen <adam@meter.com>
2025-06-26 14:58:41 -0400

    fix tests
    Had the wrong capitalization that was expected in tests.

--- codegen/testserver/usefunctionsyntaxforexecutioncontext/models.go
+++ codegen/testserver/usefunctionsyntaxforexecutioncontext/models.go
@@ -3,7 +3,7 @@ package usefunctionsyntaxforexecutioncontext
 type RoleModel string
 
 var (
-	RoleModelAdmin RoleModel = "admin"
-	RoleModelUser  RoleModel = "user"
-	RoleModelGuest RoleModel = "guest"
+	RoleModelAdmin RoleModel = "ADMIN"
+	RoleModelUser  RoleModel = "USER"
+	RoleModelGuest RoleModel = "GUEST"
 )

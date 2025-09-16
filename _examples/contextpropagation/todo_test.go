0a4a6e4533c1ec6e6df55da7926c5b948d7520b4
gintas.kovalevskis <gintas.kovalevskis@nordsec.com>
2025-09-16 15:08:44 +0300

    Test propagating context to resolvers from directive

---
+++ _examples/contextpropagation/todo_test.go
@@ -0,0 +1,33 @@
+package contextpropagation
+
+import (
+	"testing"
+
+	"github.com/99designs/gqlgen/graphql/handler/extension"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
+	"github.com/stretchr/testify/require"
+
+	"github.com/99designs/gqlgen/client"
+	"github.com/99designs/gqlgen/graphql/handler"
+)
+
+func TestTodo(t *testing.T) {
+	srv := handler.New(NewExecutableSchema(New()))
+	srv.AddTransport(transport.POST{})
+	srv.Use(extension.Introspection{})
+	c := client.New(srv)
+
+	var resp struct {
+		TestTodo struct {
+			Text    string
+			Context struct {
+				Value *string
+			}
+		}
+	}
+	c.MustPost(`{ testTodo { text context { value } } }`, &resp)
+
+	require.Equal(t, "Test", resp.TestTodo.Text)
+	require.NotNil(t, resp.TestTodo.Context.Value)
+	require.Equal(t, "Some value", *resp.TestTodo.Context.Value)
+}

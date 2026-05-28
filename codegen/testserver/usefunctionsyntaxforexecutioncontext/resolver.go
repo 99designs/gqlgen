73175f360629f51e5387243c37cd5eaedd08b4b7
Michele Prà <mpra@teleniasoftware.it>
2026-05-28 16:34:31 +0200

    fix: remove stale warning comment from usefunctionsyntaxforexecutioncontext resolver
    go generate removes this auto-generated warning block; keep the file in sync
    with what the generator produces so check-generate CI passes.

--- codegen/testserver/usefunctionsyntaxforexecutioncontext/resolver.go
+++ codegen/testserver/usefunctionsyntaxforexecutioncontext/resolver.go
@@ -50,13 +50,3 @@ func (r *Resolver) Subscription() SubscriptionResolver { return &subscriptionRes
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-/*
-	type Resolver struct{}
-*/

cdca3c04a12f0a76c8b290c469a7cfb11118a8e3
Steve Coffman <StevenACoffman@users.noreply.github.com>
2026-08-31 14:24:15 -0400

    Fix #4304: Go v1.27 any / interface{} adjustments (#4309)
    * codegen: resolve empty interfaces as any under Go 1.27
    
    * codegen: resolve empty interfaces as any under Go 1.27
    
    ---------
    
    Co-authored-by: Marcelo Mandolesi <mmandolesi@gannett.com>

--- codegen/templates/import.go
+++ codegen/templates/import.go
@@ -22,6 +22,8 @@ type Imports struct {
 	packages *code.Packages
 }
 
+var AnyType = types.Universe.Lookup("any").Type()
+
 func (i *Import) String() string {
 	if strings.HasSuffix(i.Path, i.Alias) && i.Alias == i.Name {
 		return strconv.Quote(i.Path)
@@ -118,6 +120,9 @@ func (s *Imports) Lookup(path string) string {
 }
 
 func (s *Imports) LookupType(t types.Type) string {
+	if iface, ok := types.Unalias(t).(*types.Interface); ok && iface.Empty() {
+		t = AnyType
+	}
 	return types.TypeString(t, func(i *types.Package) string {
 		return s.Lookup(i.Path())
 	})

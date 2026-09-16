1199cccaf843187669339b400482c33b8c273a74
simpleqt <89645338+simpleqt@users.noreply.github.com>
2026-09-16 18:38:25 +0800

    style: wrap the duplicate-subscription error message for golines
    Signed-off-by: simpleqt <89645338+simpleqt@users.noreply.github.com>

--- graphql/handler/transport/websocket.go
+++ graphql/handler/transport/websocket.go
@@ -471,7 +471,9 @@ func (c *wsConnection) subscribe(start time.Time, msg *message) {
 	if _, exists := c.active[msg.id]; exists {
 		c.mu.Unlock()
 		cancel()
-		c.sendError(msg.id, &gqlerror.Error{Message: fmt.Sprintf("subscription id %s is already active", msg.id)})
+		c.sendError(msg.id, &gqlerror.Error{
+			Message: fmt.Sprintf("subscription id %s is already active", msg.id),
+		})
 		c.complete(msg.id)
 		return
 	}
